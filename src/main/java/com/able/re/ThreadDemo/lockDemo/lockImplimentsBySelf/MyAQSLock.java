7775be4a0e9839b36a9e8c6a3c5d1b01f9bb1baa
cairuirui <cairuirui@@able-elec.com>
2018-09-03 17:40:27 +0800

    锁实现

---
+++ src/main/java/com/able/re/ThreadDemo/lockDemo/lockImplimentsBySelf/MyAQSLock.java
@@ -0,0 +1,72 @@
+package com.able.re.ThreadDemo.lockDemo.lockImplimentsBySelf;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * $DISCRIPTION
+ *
+ * @author cairuirui
+ * @create 2018/9/3
+ */
+public class MyAQSLock implements Lock {
+
+    private class MyAQS extends AbstractQueuedSynchronizer{
+
+        @Override
+        protected boolean tryAcquire(int arg) {
+            return super.tryAcquire(arg);
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            return super.tryRelease(arg);
+        }
+
+        @Override
+        protected int tryAcquireShared(int arg) {
+            return super.tryAcquireShared(arg);
+        }
+
+        @Override
+        protected boolean tryReleaseShared(int arg) {
+            return super.tryReleaseShared(arg);
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return super.isHeldExclusively();
+        }
+    }
+
+    @Override
+    public void lock() {
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+}
