a72e29c871e45fbfeab3657c3318cc1122a92656
cairuirui <cairuirui@flashhold.com>
2020-05-09 18:01:15 +0800

    spring自定义标签

---
+++ src/main/java/com/able/re/springResource/CustomBeanParser/Alias.java
@@ -0,0 +1,20 @@
+package com.able.re.springResource.CustomBeanParser;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Description
+ * @Created by cairuirui
+ * @Date 2020/5/9
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Alias {
+
+	private String name;
+
+	private String alias;
+}
---
+++ src/main/java/com/able/re/springResource/CustomBeanParser/CustomBeanDefinitionParper.java
@@ -0,0 +1,36 @@
+package com.able.re.springResource.CustomBeanParser;
+
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+import org.w3c.dom.Element;
+
+/**
+ * @Description
+ * @Created by cairuirui
+ * @Date 2020/5/9
+ */
+public class CustomBeanDefinitionParper extends AbstractSingleBeanDefinitionParser {
+
+	@Override
+	protected Class<?> getBeanClass(Element element) {
+		return Alias.class;
+	}
+
+	@Override
+	protected void doParse(Element element, ParserContext parserContext, BeanDefinitionBuilder builder) {
+		String beanName = element.getAttribute("c_name");
+		Assert.hasText(beanName,"the 'name' in Alias Tag is mmissing !");
+		Assert.isTrue(parserContext.getRegistry().containsBeanDefinition(beanName),"No bean name" + beanName + "exist !");
+		String c_alias = element.getAttribute("c_alias");
+		if (!StringUtils.isEmpty(c_alias)){
+			String[] split = c_alias.replaceAll("\\s+", "").split("[,;]");
+			for (String ali : split) {
+				parserContext.getRegistry().registerAlias(beanName,ali);
+			}
+		}
+
+	}
+}
