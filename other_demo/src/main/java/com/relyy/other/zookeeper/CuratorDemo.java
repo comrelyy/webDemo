c19861f31e791eccf757bb04b6f8d50192856dfb
[cairuirui] <[ruirui.cai@geekplus.com]>
2021-09-29 14:21:00 +0800

    zookeeper curator example

--- other_demo/src/main/java/com/relyy/other/zookeeper/CuratorDemo.java
+++ other_demo/src/main/java/com/relyy/other/zookeeper/CuratorDemo.java
@@ -8,6 +8,7 @@ import org.apache.curator.test.TestingServer;
 import org.apache.curator.utils.CloseableUtils;
 
 import javax.xml.ws.Service;
+import java.util.List;
 
 /**
  * @Description
@@ -15,26 +16,35 @@ import javax.xml.ws.Service;
  * @Date 2021/9/26
  */
 public class CuratorDemo {
-
+	private static final String ROOTPATH = "/example";
 	private static final String PATH = "/example/basic";
+	private static final String CONNECT = "101.35.124.40:2181";
 
 	public static void main(String[] args) throws Exception{
-		TestingServer server = new TestingServer();
+		//TestingServer server = new TestingServer();
 		CuratorFramework client = null;
 		try{
-			client = createSimple(server.getConnectString());
+			client = createSimple(CONNECT);
+			//会阻塞直到会话创建成功
 			client.start();
-			client.create().creatingParentsIfNeeded().forPath(PATH,"test".getBytes());
+
+			System.out.println(new String(client.getData().forPath(PATH)));
+			client.create().creatingParentsIfNeeded().forPath("/example/basic2","test3".getBytes());
+			//更新节点值
+			//client.setData().forPath(PATH,"test1".getBytes());
 			CloseableUtils.closeQuietly(client);
 
-			client = creatWithOptions(server.getConnectString(),new ExponentialBackoffRetry(1000,3),1000,1000);
+			client = creatWithOptions(CONNECT,new ExponentialBackoffRetry(1000,3),1000,1000);
 			client.start();
 			System.out.println(new String(client.getData().forPath(PATH)));
+			//获取所有子节点
+			List<String> strings = client.getChildren().forPath("ROOTPATH");
+			strings.stream().forEach(System.out::println);
 		}catch(Exception e){
 			e.printStackTrace();
 		}finally{
 			CloseableUtils.closeQuietly(client);
-			CloseableUtils.closeQuietly(server);
+			//CloseableUtils.closeQuietly(server);
 		}
 	}
 
