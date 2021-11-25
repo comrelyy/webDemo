fcb3139cbda32be6db4499d005a515d05b238929
relyy <relyy2014@163.com>
2021-11-25 22:25:16 +0800

    SlidingBlocks

--- game/slidingblocks/src/main/java/com/relyy/game/sildingblock/SlidingBlocks.java
+++ game/slidingblocks/src/main/java/com/relyy/game/sildingblock/SlidingBlocks.java
@@ -11,6 +11,38 @@ import java.util.Map;
  * @Date 2021/11/25
  */
 public class SlidingBlocks {
+
+	//shapeStyle.put("I",I_shape);
+	public static void main(String[] args) {
+		MyView myView = new MyView();
+
+
+		///board.paint(graphics);
+		//jPanel.repaint();
+		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
+
+class MyView extends JFrame{
+	public MyView()  {
+		board board = new board();
+		//Graphics g = jPanel.getGraphics().create();
+		//board.setBounds(120,220,300,400);
+
+		this.add(board);
+		this.setTitle("俄罗斯方块");
+		//Graphics graphics = frame.getGraphics();
+		this.setBounds(100,200,600,800);
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+
+	}
+}
+
+class board extends JPanel{
 	final static int SIDE = 15;    //每一格的像素
 	String[] shape = {"I","S","Z","L","J","o","T"};
 	Map<String,Integer[][]> shapeStyle = new HashMap<>();
@@ -18,34 +50,19 @@ public class SlidingBlocks {
 			{1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
 			{1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0},
 			{1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0}};
-	//shapeStyle.put("I",I_shape);
-	public static void main(String[] args) {
 
-		JFrame frame = new JFrame("俄罗斯方块");
-		//Graphics graphics = frame.getGraphics();
-		frame.setBounds(100,200,600,800);
-
-		board board = new board();
-		//Graphics g = jPanel.getGraphics().create();
-		board.setBounds(120,220,300,400);
-
-		frame.add(board);
-		frame.setVisible(true);
-		board.repaint();
-		///board.paint(graphics);
-		//jPanel.repaint();
-		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-	}
-
-	static class board extends JPanel{
-		public void paint(Graphics g) {
-			g.setColor(Color.green);
-			for (int i = 0; i < 16; i++) {
-				if (i_shape[0][i] == 1) {
-					g.fill3DRect((i % 4 + 150 + 1) * SIDE, (250 + i / 4) * SIDE, SIDE, SIDE, true);
-				}
+	public void paint(Graphics g) {
+		super.paint(g);
+		//g.fillRect(0,0,400,300);
+		g.setColor(Color.green);
+		for (int i = 0; i < 16; i++) {
+			if (i_shape[0][i] == 1) {
+				g.fill3DRect((i % 4 + 15 + 1) * SIDE, (0 + i / 4) * SIDE, SIDE, SIDE, true);
 			}
 		}
+	}
+
+	public class Shape{
+
 	}
 }
