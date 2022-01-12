aa733d81ebf86ce0968490da35cec65c93e60b79
relyy <relyy2014@163.com>
2022-01-12 18:36:34 +0800

    ReentrantReadWriteLock

--- jdkdemo/src/main/java/com/relyy/jdkdemo/ThreadDemo/TestRTTLock.java
+++ jdkdemo/src/main/java/com/relyy/jdkdemo/ThreadDemo/TestRTTLock.java
@@ -28,10 +28,36 @@ public class TestRTTLock {
         //获取写锁
         writeLock.lock();
         try{
+            System.out.println("进入写锁-------------"+Thread.currentThread().getName());
              x += deltaX;
              y += delraY;
-        }finally {
+             readLock.lock();
+            System.out.println(Thread.currentThread().getName()+"在写锁中进入读锁："+(x+y));
+            System.out.println("释放写锁-----"+Thread.currentThread().getName());
             writeLock.unlock();
+            Thread.sleep(5000L);
+            readLock.unlock();
+            System.out.println(Thread.currentThread().getName()+"释放读锁");
+        }catch (Exception e){
+
+        } finally {
+//            System.out.println("释放写锁-----"+Thread.currentThread().getName());
+//            writeLock.unlock();
         }
+    }
+
+    public static void main(String[] args) {
+        TestRTTLock testRTTLock = new TestRTTLock();
+        new Thread(() -> {
+            testRTTLock.move(4,3);//虽然写锁降级成读锁，但是降级之后并不是共享的
+        },"writeThread").start();
+        new Thread(() -> {
+            double read = testRTTLock.read();
+            System.out.println("read方法读取结果.........."+read);
+        },"readThread").start();
+
+//        new Thread(() -> {
+//            testRTTLock.move(6,8);
+//        },"writeThread2").start();
     }
 }
